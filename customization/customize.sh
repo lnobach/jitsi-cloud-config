89b44c21113502534733fd8493759e2f37891a3c
Leonhard Nobach <leo@nobach.net>
2021-01-08 22:12:15 +0100

    (Early) frontend customization patches

---
+++ customization/customize.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/with-contenv bash
+
+: "${CUSTOMIZE_BASE:?Shall invoke customization script, but CUSTOMIZE_BASE not set. Exiting.}"
+
+function getCustomResource() {
+    source=$1
+    target=$2
+    if ! wget -O "$target" "$CUSTOMIZE_BASE/$source"; then
+      echo "ERROR: Could not load customization resource "$source" to "$target", ignoring..."
+    fi
+}
+
+
+getCustomResource favicon.ico /usr/share/jitsi-meet/favicon.ico
+getCustomResource watermark.png /usr/share/jitsi-meet/images/watermark.png
---
+++ customization/install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+YQ_VERSION=v4.2.1
+YQ_BINARY=yq_linux_amd64
+
+set -e
+
+# === Install yq
+
+if [ -f /usr/bin/yq ]; then
+  echo "yq already present, continuing."
+else
+  echo "Getting yq first..."
+  curl -fL https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/${YQ_BINARY}.tar.gz |\
+    tar -xzO ./${YQ_BINARY} > /usr/bin/yq
+  chmod +x /usr/bin/yq
+fi
+
+# === Patch docker-compose.yaml
+
+EVALSTR='.services.web.volumes += ["/opt/app/customize/customize.sh:/etc/cont-init.d/90-customweb:ro"] | .services.web.environment += ["CUSTOMIZE_BASE"]'
+
+cp -n /opt/jitsi/docker-compose.yml /opt/jitsi/docker-compose-orig.yml || true
+yq eval "$EVALSTR" \
+  /opt/jitsi/docker-compose-orig.yml > /opt/jitsi/docker-compose.yml
